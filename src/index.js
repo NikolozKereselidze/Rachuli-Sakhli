0865ff8f425d3f1ee1e05f64fe5aad954a023fe4
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-05-04 17:40:20 +0400

    feat: create home page layout and styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { initialize } from "./initialize";
+import { initializeMenu } from "./menu";
 import "./css/style.css";
 
+
+
 initialize();
+initializeMenu();
--- src/initialize.js
+++ src/initialize.js
@@ -1,19 +1,27 @@
-function createNav() {
+export function createNav() {
   const navContent = document.createElement("header");
   navContent.classList.add("header");
 
   const homeButton = createButton();
   homeButton.textContent = "Home";
+  homeButton.classList.add("active");
   const menuButton = createButton();
   menuButton.textContent = "Menu";
   const aboutButton = createButton();
   aboutButton.textContent = "About";
 
+  addBtnNavClass(homeButton, "home-button");
+  addBtnNavClass(menuButton, "menu-button");
+  addBtnNavClass(aboutButton, "about-button");
   navContent.appendChild(homeButton);
   navContent.appendChild(menuButton);
   navContent.appendChild(aboutButton);
 
   return navContent;
+}
+
+function addBtnNavClass(btn, btnClass) {
+  btn.classList.add("nav-button", `${btnClass}`);
 }
 
 function createMain() {
